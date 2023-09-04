f0c8989f79ada9a1b3382d95eb3a75323dd439fe
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-04 17:18:19 -0300

    feat(department): implement insert

--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,14 +1,47 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.sql.*;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Department obj) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("INSERT INTO department (Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getName());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            } else {
+                throw new DbException("Unexpected error: no rows affected");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
