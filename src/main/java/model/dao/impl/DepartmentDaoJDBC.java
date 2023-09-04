7e5a6f13a9e1339b77303492518c7ff278a8eae5
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-04 17:30:29 -0300

    feat(department): implement delete by id

--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -90,7 +90,22 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try{
+            st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+            st.setInt(1, id);
+
+            int rows = st.executeUpdate();
+            if (rows == 0){
+                throw new DbException("ID NOT FOUND. Try again with a different id");
+            }
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
