dda1382ad23cba26088c820106e848e1dc1aa18d
Matheus Gomes <maatheus.gsr@gmail.com>
2023-09-04 18:11:42 -0300

    feat(department): implement find all

--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -34,6 +35,33 @@ public class DepartmentDaoJDBC implements DepartmentDao {
                 return obj;
             }
             return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Department> findAll() {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+
+            while (rs.next()) {
+                Department obj = new Department();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                list.add(obj);
+            }
+            return list;
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
         } finally {
@@ -106,10 +134,5 @@ public class DepartmentDaoJDBC implements DepartmentDao {
         }finally {
             DB.closeStatement(st);
         }
-    }
-
-    @Override
-    public List<Department> findAll() {
-        return null;
     }
 }
